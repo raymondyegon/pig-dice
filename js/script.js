867fd9b8a27a8b6252686faeb15ecdd7fd06f88c
Raymond Yegon <raymondyegon17@gmail.com>
2019-03-03 17:29:19 +0300

    Function to keep adding gained numbers

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 //business logic
+function Player(turn){
+  this.totalscore=0;
+  this.gains=0
+}
 
 /*this function runs a random numbers between 1 and 6 while displaying it*/
 function rolling() {
@@ -9,12 +13,33 @@ function roll(){
   var ran =Math.floor(1 + Math.random()*6);
   document.getElementById('dice').innerHTML=ran
 }
+//function to keep adding rolling numbers
+var gains = 0;
+
+function ad(a){
+  return gains += a
+ }
+
+var player1 ='';
+var player2='''
+
+Player.prototype.stop = function () {
+
+}
 
 //Function to stop the rolling
 function stop(){
   clearInterval(myVar);
+  var num = parseInt(document.getElementById('dice').innerHTML)
+  if (num == 1) {
+    document.getElementById('gains').innerHTML='0'
+  } else {
+    document.getElementById('gains').innerHTML= ad(num)
+  }
 }
+
+//function that gets the randomized number
 function number() {
-  var num = document.getElementById('dice').innerHTML
-  return alert (num)
 }
+
+//
